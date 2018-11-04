0ca9f4f8342ba603e4488937660f45ad4a8a36bb
Steven Thomason <thomason84@hotmail.com>
2018-11-03 21:19:52 -0400

    added jQuery and updated css

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
 console.log('Hello Hackers ;)');
+$(document).ready(function() {
+
+  // calls to links in the navbar
+  $('#home').click(function(){
+    window.location = 'http://charlestonhackers.com';
+  });
+
+  $('#projects').click(function(){
+    window.location = 'http://charlestonhackers.com/projects.html';
+  });
+  $('#about').click(function(){
+    console.log('clicked');
+    window.location = 'http://charlestonhackers.com/about.html';
+  });
+
+
+});
