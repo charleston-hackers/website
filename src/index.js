1b617b42d976eb532c93ed179130267f0f0d1067
thomason84 <thomason84@me.com>
2018-11-04 12:02:32 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,7 @@
 console.log('Hello Hackers ;)');
 $(document).ready(function() {
 
-  // calls to links in the navbar
-  $('#home').click(function(){
-    window.location = 'http://charlestonhackers.com';
-  });
-
-  $('#projects').click(function(){
-    window.location = 'http://charlestonhackers.com/projects.html';
-  });
-  $('#about').click(function(){
-    console.log('clicked');
-    window.location = 'http://charlestonhackers.com/about.html';
-  });
+ 
 
 
 });
